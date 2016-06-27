e5c656b79f5ab86675d0ba89deb7f79567692043
Ryan Tsao <rtsao@uber.com>
2016-06-26 19:29:05 -0700

    Fix async callback

--- index.js
+++ index.js
@@ -77,22 +77,26 @@ function rebasePaths(entry, outDir) {
 }
 
 function copyPackageMeta(baseDir, destDir, callback) {
-  var files = [];
+  var callbacks = [];
   fs.readdir(baseDir, function(err, files) {
     files.forEach(function(file) {
       var absPath = path.join(baseDir, file);
+      var done;
+      callbacks.push(function(cb) {
+        done = cb;
+      });
       fs.stat(absPath, function(err, stats) {
         if (stats.isFile() && isPackageMeta(file)) {
           var out = fs.createWriteStream(path.join(destDir, file));
-          files.push(function(cb) {
-            out.on('finish', cb);
-          });
+            out.on('finish', done);
           fs.createReadStream(absPath).pipe(out);
+        } else {
+          done();
         }
       });
     });
+    parallel(callbacks, callback);
   });
-  parallel(files, callback);
 }
 
 /*
